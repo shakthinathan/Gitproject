b8ad53ca8b1f587b3c4b91e6d56f4027aa4c949a
shakthinathan <shakthinathan488@gmail.com>
2021-09-01 12:39:27 +0530

    File changes

--- src/main/java/JunitTestsGradle/SumNumbers.java
+++ src/main/java/JunitTestsGradle/SumNumbers.java
@@ -4,8 +4,8 @@ public class SumNumbers {
 
 	public int sum(int a, int b)
 	{
-		if(a>=10) {
-			throw new IllegalArgumentException("Value should be less than 10");
+		if(a>=12) {
+			throw new IllegalArgumentException("Value should be less than 12");
 		
 		}
 		return a+b;
